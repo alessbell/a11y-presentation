4a0153389d9a7bcc3b3f6617ec884d7153529339
Alessia Bellisario <abellisario@paypal.com>
2021-01-27 13:00:02 -0500

    Morning edits

--- site.config.js
+++ site.config.js
@@ -1,7 +1,7 @@
 export const siteConfig = {
   name: `Alessia Bellisario`,
   title: `Web a11y: Front-end Knowledge Exchange`,
-  date: `January 24, 2021`,
+  date: `January 27, 2021`,
   author: {
     url: `https://twitter.com/alessbell`,
   },
