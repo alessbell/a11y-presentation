97e94addc4fa453cd53937a947b466def3d7b192
Alessia Bellisario <abellisario@paypal.com>
2021-01-27 09:31:18 -0500

    Remove .next

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/slides/[slide]":["static\u002Fchunks\u002Fpages\u002Fslides\u002F[slide].js"],sortedPages:["\u002F_app","\u002F_error","\u002Fslides\u002F[slide]"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
